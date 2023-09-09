de8849f12045792a5c7f08cb5557e37eb9e131d4
Yota8 <1602203689abcd@gmail.com>
2023-09-09 20:20:00 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PermissionT
 a library for easily request permissions (android)
+
+```
+# Language
+- de_DE [Deutsch](readme/README.de_DE.md)
+- en [English](README.md)
+- es_ES [Español](readme/README.es_ES.md)
+- fr_FR [Français](readme/README.fr_FR.md)
+- hy [Հայերեն](readme/README.hy.md)
+- pt_BR [Português do Brasil](readme/README.pt_BR.md)
+- ru [Русский](readme/README.ru.md)
+- sv_SE [Svenska](readme/README.sv_SE.md)
+- jp [日本語](readme/README.jp.md)
+- zh_CN [简体中文](readme/README.zh_CN.md)
+- zh_TW [繁体中文](readme/README.zh_TW.md)
+```
--- app/src/main/java/com/yota8dev/permissiont/MainActivity.kt
+++ app/src/main/java/com/yota8dev/permissiont/MainActivity.kt
@@ -13,18 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button).setOnClickListener {
-            PermissionT.request(this, Manifest.permission.CALL_PHONE, Manifest.permission.READ_CONTACTS) { allGranted, deniedList ->
-                run {
-                    if (allGranted) {
-                        gerPermission()
-                    } else {
-                        Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
-                    }
+            /* English:
+            * You can obtain permissions through the following syntax.
+            * The second parameter (Manifest. permission. XXX) can be any number of permissions,
+            * and the lambda expression is used for processing permission requests
+            * */
+
+            /* Chinese:
+            * 您可以通过如下语法来获取权限.
+            * 第二个参数(Manifest.permission.XXX)可以为任意个数的权限,
+            * lambda表达式中进行申请权限的处理
+            * */
+            PermissionT.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    gerPermission()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
+    // call phone permission
     private fun gerPermission() {
         try {
             val intent = Intent(Intent.ACTION_CALL)
